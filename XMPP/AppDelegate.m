cddeda6de1743954c75f7c41339aa33324db914e
xiao <ilife_x@163.com>
2021-02-22 11:06:29 +0800

    init

---
+++ XMPP/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  XMPP
+//
+//  Created by xiao on 2021/2/22.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    return YES;
+}
+
+
+
+
+
+@end
---
+++ XMPP/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  XMPP
+//
+//  Created by xiao on 2021/2/22.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = UIColor.whiteColor;
+}
+
+
+@end
